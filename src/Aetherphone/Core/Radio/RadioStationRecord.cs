49d9c9cd85dc517d4d7bf6c23874308ba48e329f
Hubkaw <hubkaw@gmail.com>
2026-07-22 17:18:36 +0200

    add serializable to RadioStationRecord

--- src/Aetherphone/Core/Radio/RadioStationRecord.cs
+++ src/Aetherphone/Core/Radio/RadioStationRecord.cs
@@ -1,17 +1,13 @@
 namespace Aetherphone.Core.Radio;
 
+[Serializable]
 internal sealed class RadioStationRecord
 {
     public string Uuid { get; set; } = string.Empty;
-
     public string Name { get; set; } = string.Empty;
-
     public string StreamUrl { get; set; } = string.Empty;
-
     public string Codec { get; set; } = string.Empty;
-
     public int Bitrate { get; set; }
-
     public string Country { get; set; } = string.Empty;
 
     public static RadioStationRecord From(RadioStation station)
